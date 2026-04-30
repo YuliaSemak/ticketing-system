2d67fe4b5ec1f59dbb062d97cf9fcb5f9d744827
YuliaSemak <yuliia.a.semak@ukd.edu.ua>
2026-04-30 12:33:27 +0300

    server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
-/**
- * server.js — Express сервер для системи продажу квитків
- * Запуск: node server.js  →  http://localhost:3000
- */
-
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 3000;
 
-// Статичні файли з папки public
-// Було:
 app.use(express.static(path.join(__dirname, 'public')));
-// Стало (просто поточна папка):
 app.use(express.static(__dirname));
 
-// Було:
 res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// Стало:
 res.sendFile(path.join(__dirname, 'index.html'));
 
-// Головна сторінка
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
